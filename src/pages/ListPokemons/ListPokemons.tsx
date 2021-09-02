449f596f12d8a065c999181cf917f048d002863e
joaovictorpsantos <joaovictorsantoscpv@gmail.com>
2021-09-01 22:37:54 -0300

    feature: pokemon list page structure

--- src/pages/ListPokemons/ListPokemons.tsx
+++ src/pages/ListPokemons/ListPokemons.tsx
@@ -1,3 +1,43 @@
+import { Layout, Typography, Input, Row, Col } from 'antd';
+
+import './listPokemons.css';
+
 export const ListPokemons = () => {
-  return <h1>lista</h1>;
+  const { Header, Content } = Layout;
+  const { Title, Text } = Typography;
+  const { Search } = Input;
+
+  return (
+    <Layout>
+      <Header style={{ backgroundColor: '#fff' }}>
+        <Title level={1}>Pokdex</Title>
+      </Header>
+      <Content>
+        <Search placeholder="Digite o nome do pokemon" />
+
+        <Row gutter={[0, 20]} justify="space-around">
+          <Col className="pokemon-card">
+            <Text>Nome</Text>
+            <img src="" alt="" />
+          </Col>
+          <Col className="pokemon-card">
+            <Text>Nome</Text>
+            <img src="" alt="" />
+          </Col>
+          <Col className="pokemon-card">
+            <Text>Nome</Text>
+            <img src="" alt="" />
+          </Col>
+          <Col className="pokemon-card">
+            <Text>Nome</Text>
+            <img src="" alt="" />
+          </Col>
+          <Col className="pokemon-card">
+            <Text>Nome</Text>
+            <img src="" alt="" />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
 };
